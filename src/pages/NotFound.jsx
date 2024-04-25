682980fb0f507d3e31ad992cc47cc70090731254
htmil <emil.cederslatt@gmail.com>
2024-04-26 01:04:56 +0200

    small changes

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,8 +4,6 @@ export default function NotFound() {
   return (
     <div>
         <h2>Page not found!</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium sit, impedit maxime qui commodi facilis obcaecati fugiat itaque voluptates molestias </p>
-
         <p>Go to the <Link to="/" >Homepage</Link>.</p>
     </div>
   )
